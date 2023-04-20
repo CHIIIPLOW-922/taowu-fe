7496980d549e54e9cf551b4a4de667fdcec8778a
YangZhiXiong <641484973@qq.com>
2023-04-20 22:05:57 +0800

    接口调整

--- taowu-admin/vue.config.js
+++ taowu-admin/vue.config.js
@@ -3,11 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:'./',
   devServer:{
-    port:3100,
+    port:9000,
     open: true,
       proxy: {
         '/api': {
-          target: 'http://localhost:3011', // 本地后端地址
+          target: 'http://localhost:8000', // 本地后端地址
           //target: 'http://106.15.179.105:3000/', // 线上后端地址
           changeOrigin: true, //允许跨域
           pathRewrite: {
